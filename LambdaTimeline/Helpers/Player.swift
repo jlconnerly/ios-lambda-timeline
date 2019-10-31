fdbb78ccc34d17c94ff048b48d952e910b34e820
Jake <jlconnerly@gmail.com>
2019-10-31 01:39:36 -0500

    added AVAudioPlayer

--- LambdaTimeline/Helpers/Player.swift
+++ LambdaTimeline/Helpers/Player.swift
@@ -10,12 +10,82 @@ import AVFoundation
 
 protocol PlayerDelegate {
     func playerDidChangeState(player: Player)
+    
 }
 
 class Player: NSObject {
     
     var audioPlayer: AVAudioPlayer?
+    var delegate: PlayerDelegate?
+    var url: URL?
+    var timer: Timer?
+    
+    var timeElapsed: TimeInterval {
+        audioPlayer?.currentTime ?? 0.0
+    }
+    
+    var duration: TimeInterval {
+        audioPlayer?.duration ?? 0.0
+    }
+    
+    var timeRemaining: TimeInterval {
+        duration - timeElapsed
+    }
+    
+    override init() {
+        super.init()
+        audioPlayer?.delegate = self
+    }
+    
+    var isPlaying: Bool {
+        return audioPlayer?.isPlaying ?? false
+    }
+    
+    func play() {
+        audioPlayer?.play()
+        delegate?.playerDidChangeState(player: self)
+        startTimer()
+    }
+    
+    func pause() {
+        audioPlayer?.pause()
+        delegate?.playerDidChangeState(player: self)
+        cancelTimer()
+    }
+    
+    func playPause() {
+        if isPlaying {
+            pause()
+        } else {
+            play()
+        }
+    }
+    
+    func startTimer() {
+        cancelTimer()
+        timer = Timer.scheduledTimer(timeInterval: 0.03, target: self, selector: #selector(updateTimer(timer:)), userInfo: nil, repeats: true)
+    }
+
+    func cancelTimer() {
+        timer?.invalidate()
+        timer = nil
+    }
+
+    @objc private func updateTimer(timer: Timer) {
+        delegate?.playerDidChangeState(player: self)
+    }
     
     
+}
+extension Player: AVAudioPlayerDelegate {
+    func audioPlayerDecodeErrorDidOccur(_ player: AVAudioPlayer, error: Error?) {
+        print("AVAudioError: \(String(describing: error))")
+        cancelTimer()
+    }
     
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        // send delegate message to update the UI
+        delegate?.playerDidChangeState(player: self)
+        cancelTimer()
+    }
 }
